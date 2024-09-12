3e0fb6db299fff75f6bbe385b6f9df5f15d9b6b8
Ilesthg <vhgm@hotmail.com>
2024-09-11 21:05:12 -0700

    Fixing

---
+++ Java_Proj/src/Archivos/jol.java
@@ -0,0 +1,7 @@
+package Archivos;
+
+public class jol {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
---
+++ Java_Proj/src/Metodos/a.java
@@ -0,0 +1,24 @@
+
+package Java_Proj.src.Metodos;
+
+public class a {
+
+    public static void main(String[] args) {
+        MetodoStirngs.metodoStrings("a", "b");
+
+        int matriz1[][] = new int[3][2];
+
+        matriz1[0][0] = 1;
+        matriz1[0][1] = 2;
+        matriz1[1][0] = 3;
+        matriz1[1][1] = 4;
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+
+                System.out.println(matriz1[i][j]);
+            }
+        }
+
+    }
+
+}
